347d5db1a7b759b0a2531e6e923ca36b9fab9b4d
knmurthy <knmurthy@outlook.com>
2016-10-01 16:59:23 +0530

    Revert "Dashboard: Change activity for new Superuser"
    This reverts commit ee3b7c0edcdc309b18ac805203506bc1e1daf5a9.

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -1011,7 +1011,7 @@ public class SettingsActivity extends SettingsDrawerActivity
             boolean addToBackStack, int titleResId, CharSequence title, boolean withTransition) {
         if (SUPERSU_FRAGMENT.equals(fragmentName)) {
             Intent superSUIntent = new Intent();
-            superSUIntent.setClassName("me.phh.superuser", "com.koushikdutta.superuser.MainActivity");
+            superSUIntent.setClassName("eu.chainfire.supersu", "eu.chainfire.supersu.MainActivity");
             startActivity(superSUIntent);
             finish();
             return null;
@@ -1103,7 +1103,7 @@ public class SettingsActivity extends SettingsDrawerActivity
         // Embedding into Settings is supported from SuperSU v1.85 and up
         boolean suSupported = false;
         try {
-            suSupported = (getPackageManager().getPackageInfo("me.phh.superuser", 0).versionCode >= 1033);
+            suSupported = (getPackageManager().getPackageInfo("eu.chainfire.supersu", 0).versionCode >= 185);
         } catch (PackageManager.NameNotFoundException e) {
         }
         setTileEnabled(new ComponentName(packageName,
